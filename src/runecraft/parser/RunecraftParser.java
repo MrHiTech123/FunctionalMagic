9a0a9b99ca1f3108bbcf7790230d23b8622fef11
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2025-10-28 13:28:55 -0400

    Added number parsing

---
+++ src/runecraft/parser/RunecraftParser.java
@@ -0,0 +1,87 @@
+package runecraft.parser;
+
+import runecraft.result.*;
+
+public abstract class RunecraftParser {
+    
+    
+    public boolean compareToken(String tokens, String target) {
+        String firstTokens = tokens.substring(0, target.length());
+        return firstTokens.equals(target);
+        
+    }
+    
+    abstract void shoot(RunecraftObjectResult objectShot);
+    
+    public RunecraftIntResult parseNumber(String tokens) {
+        System.out.println(tokens);
+        RunecraftResult numberParsed;
+        if (tokens.isEmpty()) {
+            return new RunecraftIntResult(0, "");
+        }
+        if (compareToken(tokens, "🝰")) {
+            numberParsed = parseNumber(tokens.substring("🝰".length()));
+        }
+        else if (compareToken(tokens, "🝯")) {
+            numberParsed = parseNumber(tokens.substring("🝯".length()));
+        }
+        // else if (compareToken(tokens, ".")) {
+        //     numberParsed = new RunecraftIntResult(0, tokens.substring(".".length()));
+        // }
+        else {
+            numberParsed = new RunecraftIntResult(0, tokens);
+        }
+        
+        int resultNum = (numberParsed.resultInt() * 2);
+        if (compareToken(tokens,"🝯")) {
+            ++resultNum;
+        }
+        return new RunecraftIntResult(resultNum, numberParsed.remainingTokens());
+    }
+    
+    
+    public RunecraftResult parse(String tokens) {
+        if (tokens.isEmpty()) {
+            return new RunecraftErrorResult("Expected expression, found nothing");
+        }
+        RunecraftResult result;
+        if (compareToken(tokens, "🜂")) {
+            result = new RunecraftObjectResult("Fire", tokens.substring("🜂".length()));
+        }
+        
+        else if (compareToken(tokens, "🝏")) {
+            RunecraftResult substanceOfBolt = parse(tokens.substring("🝏".length()));
+            return new RunecraftObjectResult("(boltOf " + substanceOfBolt.resultString() + ")", substanceOfBolt.remainingTokens());
+        }
+        
+        else if (compareToken(tokens, "🝭")) {
+            String leftoverTokens = tokens.substring("🝭".length());
+            
+            RunecraftResult objectShot = parse(leftoverTokens);
+            if (objectShot instanceof RunecraftObjectResult runecraftObjectResult) {
+                shoot(runecraftObjectResult);
+            }
+            
+            result = new RunecraftEmptyResult(objectShot.remainingTokens());
+            
+        }
+        else if (compareToken(tokens, "🜼")) {
+            String argumentTokens = tokens.substring("🜼".length());
+            RunecraftResult firstResult = parse(argumentTokens);
+            RunecraftResult secondResult = parse(firstResult.remainingTokens());
+            result = new RunecraftEmptyResult(secondResult.remainingTokens());
+        }
+        else if (compareToken(tokens, "🝰") || compareToken(tokens, "🝯")) {
+            result = parseNumber(tokens);
+        }
+        else {
+            result = new RunecraftErrorResult("Error: Unknown character");
+        }
+        if (result instanceof RunecraftErrorResult) {
+            System.out.println("Error");
+        }
+        return result;
+    }
+    
+    
+}
---
+++ src/runecraft/parser/RunecraftPrinterParser.java
@@ -0,0 +1,21 @@
+package runecraft.parser;
+
+import runecraft.result.RunecraftErrorResult;
+import runecraft.result.RunecraftObjectResult;
+import runecraft.result.RunecraftResult;
+
+public class RunecraftPrinterParser extends RunecraftParser {
+    public void shoot(RunecraftObjectResult result) {
+        System.out.println("(shoot " + result.resultString() + ")");
+    }
+    
+    public static void main(String[] args) {
+        RunecraftPrinterParser parser = new RunecraftPrinterParser();
+        parser.parse("🜼🜼🝭🜂🝭🝏🜂🝭🝏🝏🜂");
+        RunecraftResult result = parser.parse("🝰🝯🝰🝯");
+        if (result instanceof RunecraftErrorResult error) {
+            System.out.println(error.resultString());
+        }
+        System.out.println(result.resultInt());
+    }
+}
