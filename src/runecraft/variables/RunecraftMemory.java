a9ab6d428e5ae77373a4e446e1aacb77b26e6226
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2025-11-08 21:47:03 -0500

    Fixed errors

--- src/runecraft/variables/RunecraftMemory.java
+++ src/runecraft/variables/RunecraftMemory.java
@@ -5,13 +5,16 @@ import runecraft.result.RunecraftResult;
 import java.util.*;
 
 public class RunecraftMemory {
-    private static String[]
+    private static String GLOBAL_LETTERS = "ⳎⲰⲈⲢⲦⲨⳘⲒⲞⲠⲀⲊⲆⲜⲄⲎⳢⲔⲖⲌⲬⲤⳔⲂⲚⲘ";
+    private static String LOCAL_LETTERS = "ⳏⲱⲉⲣⲧⲩⳙⲓⲟⲡⲁⲋⲇⲝⲅⲏⳣⲕⲗⲍⲭⲥⳕⲃⲛⲙ";
+    
     
     private Map<String, RunecraftResult<?>> GLOBALS = new HashMap<>();
     private Map<String, Queue<?>> LOCALS = new HashMap<>();
     
     public static void main(String[] args) {
         Queue<Integer> objects = Collections.asLifoQueue(new LinkedList<>());
+        System.out.println(GLOBAL_LETTERS.length());
     }
     
 }
